b2a0c1320f2f66814dd7b94e3eea4e4ce5461c7b
AlexGarmacia <alexandrugarmavalerian@gmail.com>
2025-07-25 18:30:56 +0200

    js

---
+++ Web_2/cambionome.js
@@ -0,0 +1,16 @@
+import React from "react"
+const CambioNome = () => {
+    const [nome,setNome] = useState("Roberto")
+        const toggleName=()=>{
+            if(nome=="Roberto"){
+                setNome("Mario")
+            }else{
+                setNome("Roberto")
+            }
+        }
+    return (<div>
+        <h2>{nome}</h2>
+        <button onClick={toggleName} className="btn btn-dark">Cambia Nome</button>
+    </div>)
+}
+export default CambiaNome;
